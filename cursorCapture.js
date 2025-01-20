88651cb20ea1c1880d0d9f16b8120b516a3028a2
sinano1107 <tftf161107@icloud.com>
2025-01-20 16:16:39 +0900

    update comment

--- cursorCapture.js
+++ cursorCapture.js
@@ -1,5 +1,5 @@
 // 艦これCanvas内でのカーソルの座標をログに出力する
-// こっちで使える→http://w14h.kancolle-server.com/kcs2/index.php?api_root=/kcsapi&voice_root=/kcs/sound&osapi_root=osapi.dmm.com&version=6.0.1.1&api_token=16d46fabd2ff24c72bddeeac0c877bed6d210118&api_starttime=1737300469815
+// canvasを含むiframeのsrcに書かれているURLにアクセスして、consoleで実行すると使えます
 
 const canvas = document.getElementsByTagName("canvas")[0];
 canvas.onmousemove = (e) => console.log(e.offsetX, e.offsetY);
