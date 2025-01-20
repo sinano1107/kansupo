86170f9b0cb696e6b3484fcdfa845cd33cbd60e5
sinano1107 <tftf161107@icloud.com>
2025-01-20 15:11:56 +0900

    update screenshotコマンドを実行できるようにした

--- main.py
+++ main.py
@@ -1,27 +1,63 @@
-from playwright.sync_api import sync_playwright, Playwright
+from playwright.sync_api import sync_playwright
+import threading
+import time
+import queue
+from datetime import datetime
 from login import login
 from click import click
 
 
-def run(playwright: Playwright):
-    chromium = playwright.chromium
-    browser = chromium.launch(headless=False)
-    page = browser.new_page()
+class ThreadJob(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self, daemon=True)
+        self.kill_flag = False
+        self.queue = queue.Queue()
+        self.canvas = None
 
-    login(page)
+    def run(self):
+        with sync_playwright() as playwright:
+            chromium = playwright.chromium
+            browser = chromium.launch(headless=False)
+            page = browser.new_page()
 
-    page.wait_for_timeout(15000)
-    canvas = (
-        page.locator('iframe[name="game_frame"]')
-        .content_frame.locator("#htmlWrap")
-        .content_frame.locator("canvas")
-    )
+            login(page)
 
-    click(canvas, x_range=(700, 1100), y_range=(560, 640))
+            page.wait_for_timeout(15000)
+            self.canvas = (
+                page.locator('iframe[name="game_frame"]')
+                .content_frame.locator("#htmlWrap")
+                .content_frame.locator("canvas")
+            )
 
-    page.wait_for_timeout(100000)
-    browser.close()
+            self.click(x_range=(700, 1100), y_range=(560, 640))
+
+            page.wait_for_timeout(10000)
+
+            while True:
+                if not self.queue.empty():
+                    command = self.queue.get()
+                    if command == "screenshot":
+                        self.screenshot()
+                time.sleep(1)
+
+    def click(self, x_range: tuple[float, float], y_range: tuple[float, float]):
+        click(self.canvas, x_range, y_range)
+
+    def screenshot(self):
+        if self.canvas == None:
+            print("canvasがNoneなのでスクリーンショットを取得できません")
+        else:
+            self.canvas.screenshot(path="screenshots/{}.png".format(datetime.now()))
 
 
 with sync_playwright() as playwright:
-    run(playwright)
+    t = ThreadJob()
+    t.start()
+
+    while True:
+        command = input()
+        if command == "screenshot":
+            t.queue.put("screenshot")
+            print("screenshotをqueueに追加しました")
+        else:
+            print("不明なコマンドです {}".format(command))
