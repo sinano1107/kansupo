1f00cd5199aeccd272bda82903b85f651174b2f3
sinano1107 <tftf161107@icloud.com>
2025-01-27 11:00:07 +0900

    update scanの類似度抽出を効率化 update scanのログ表示フラグを追加

--- detect_huge_damage.py
+++ detect_huge_damage.py
@@ -8,25 +8,29 @@ def detect_huge_damage(canvas: Locator):
     return detect_huge_damage_without_screenshot(path=path)
 
 
-def detect_huge_damage_without_screenshot(path: str):
+def detect_huge_damage_without_screenshot(path: str, log=False):
     img = cv.imread(path)
     template = cv.imread("scan_targets/images/huge_damage.png")
     res: list[int] = []
     for i in range(6):
         y_start = 68 * i + 305
         cropped = img[y_start : y_start + 18, 484:519]
-        similarity = cv.matchTemplate(cropped, template, cv.TM_CCOEFF_NORMED)
-        _, max_val, _, _ = cv.minMaxLoc(similarity)
-        # print("similarity: ", max_val)
-        if max_val > 0.9:
+        similarity = cv.matchTemplate(cropped, template, cv.TM_CCOEFF_NORMED)[0][0]
+        if log:
+            print("similarity: ", similarity)
+        if similarity > 0.9:
             res.append(i)
     return res
 
 
 if __name__ == "__main__":
-    print(detect_huge_damage_without_screenshot("test/detect_huge_damage/1.png"))
-    print(detect_huge_damage_without_screenshot("test/detect_huge_damage/2.png"))
-    print(detect_huge_damage_without_screenshot("test/detect_huge_damage/3.png"))
-    print(detect_huge_damage_without_screenshot("test/detect_huge_damage/4.png"))
-    print(detect_huge_damage_without_screenshot("test/detect_huge_damage/5.png"))
-    print(detect_huge_damage_without_screenshot("test/detect_huge_damage/6.png"))
+    paths = [
+        "test/detect_huge_damage/1.png",
+        "test/detect_huge_damage/2.png",
+        "test/detect_huge_damage/3.png",
+        "test/detect_huge_damage/4.png",
+        "test/detect_huge_damage/5.png",
+        "test/detect_huge_damage/6.png",
+    ]
+    for path in paths:
+        print(detect_huge_damage_without_screenshot(path, log=True))
--- scan.py
+++ scan.py
@@ -10,7 +10,7 @@ class ScanTarget:
         self.IMAGE = image
 
 
-def scan(canvas: Locator, targets: list[ScanTarget]) -> int:
+def scan(canvas: Locator, targets: list[ScanTarget], log=False) -> int:
     canvas.screenshot(path="screenshots/use-for-scan.png")
     image = cv2.imread("screenshots/use-for-scan.png")
     for i, target in enumerate(targets):
@@ -18,11 +18,12 @@ def scan(canvas: Locator, targets: list[ScanTarget]) -> int:
             target.RECTANGLE.Y_RANGE[0] : target.RECTANGLE.Y_RANGE[1],
             target.RECTANGLE.X_RANGE[0] : target.RECTANGLE.X_RANGE[1],
         ]
+        # croppedとtarget.IMAGEのサイズは同じ前提なので、類似度は1x1の配列で返ってくる
         similarity = cv2.matchTemplate(
             cropped, cv2.imread(target.IMAGE), cv2.TM_CCOEFF_NORMED
-        )
-        _, max_val, _, _ = cv2.minMaxLoc(similarity)
-        # print(i, " similarity: ", max_val)
-        if max_val > 0.9:
+        )[0][0]
+        if log:
+            print("{}番目のターゲットとの類似度は{}です".format(i, similarity))
+        if similarity > 0.9:
             return i
     return -1
