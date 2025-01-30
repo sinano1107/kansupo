b9a2d47ca71ae1b9572c9ab1b8b901a11fd21b8e
sinano1107 <tftf161107@icloud.com>
2025-01-30 23:47:15 +0900

    update record_response.pyを改良

--- record_response.py
+++ record_response.py
@@ -7,7 +7,7 @@ from datetime import datetime
 # ゲームプレイ中のレスポンスをtxtファイルとして記録する
 
 API_ROOT_PATH = "http://w14h.kancolle-server.com/kcsapi/"
-async def record_response(res: Response, start_date: datetime):
+async def record_response(res: Response, name: str):
     if not res.url.startswith(API_ROOT_PATH):
         return
     
@@ -15,29 +15,27 @@ async def record_response(res: Response, start_date: datetime):
     text = await res.text()
     data = json.loads(text[7:])
     
-    if not os.path.isdir(f"responses/{start_date}"):
-        os.makedirs(f"responses/{start_date}")
+    if not os.path.isdir(f"responses/{name}"):
+        os.makedirs(f"responses/{name}")
     
-    with open(f"responses/{start_date}/{datetime.now()}_{api_name.replace("/", "-")}.txt", "w") as f:
+    with open(f"responses/{name}/{datetime.now()}_{api_name.replace("/", "-")}.json", "w") as f:
         f.write(json.dumps(data, indent=4, ensure_ascii=False))
         print(api_name + "のレスポンスを記録しました")
 
 async def main():
-    async with async_playwright() as p:
-        start_date = datetime.now()
-        browser = await p.chromium.launch(headless=False)
-        context = await browser.new_context(storage_state="login_account.json")
-        p_page = await context.new_page()
-        p_page.on("response", lambda res: record_response(res, start_date))
-        await p_page.goto("http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854")
-        
-        while True:
-            await asyncio.sleep(1)
+    name = input("レコード名を入力してください: ")
     
-    while True:
-        for i in range(4):
-            print("\rwaiting" + "." * i + " " * (3 - i), end="")
-            await asyncio.sleep(1)
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        context = await browser.new_context(storage_state="login_account.json", record_video_dir=f"responses/{name}")
+        p_page = await context.new_page()
+        p_page.on("response", lambda res: record_response(res, name))
+        await p_page.goto("http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854")
+    
+        while True:
+            for i in range(4):
+                print("\rworking" + "." * i + " " * (3 - i), end="")
+                await asyncio.sleep(1)
 
 if __name__ == "__main__":
     asyncio.run(main())
