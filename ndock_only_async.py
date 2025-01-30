15f5d40b43501ee61812d97428cb39dc4d61bfe0
sinano1107 <tftf161107@icloud.com>
2025-01-30 09:53:31 +0900

    update ndock_only_async.py

--- ndock_only_async.py
+++ ndock_only_async.py
@@ -3,35 +3,62 @@ from enum import Enum
 import json
 import operator
 from random import random
-from typing import Callable, Coroutine
+from typing import Coroutine
+import cv2
+import numpy as np
 from playwright.async_api import async_playwright, Response, Locator
 
 from rectangle import Rectangle
+from scan import ScanTarget
+from scan_targets.index import SETTING_SCAN_TARGET
 from targets import GAME_START, HOME_PORT, REPAIR, REPAIR_START, REPAIR_START_CONFIRM, repair_dock_button, repair_ship
 
 class Page(Enum):
-    WAITING_START = "WAITING_START"
     START = "START"
     PORT = "PORT"
     NDOCK = "NDOCK"
 
-page: Page = Page.WAITING_START
+page: Page = Page.START
 canvas: Locator = None
-handle_in_ndock_page: Callable[[], Coroutine] = lambda: asyncio.sleep(0)
+wait_for_dock_to_open_task: asyncio.Task = None
+queue = asyncio.Queue[Coroutine]()
 
 async def random_sleep(base = 1, buffer = 1):
+    """ランダムな秒数待つ"""
     await asyncio.sleep(base + random() * buffer)
 
 async def click(canvas: Locator, target=Rectangle(x_range=(0, 1200), y_range=(0, 720))):
+    """指定された範囲のランダムな位置をクリックする"""
     x, y = target.random_point()
     await canvas.click(position={"x": x, "y": y})
 
+async def scan(canvas: Locator, targets: list[ScanTarget], log=False) -> int:
+    """画面内の指定されたターゲットとの類似度を比較する"""
+    screenshot = await canvas.screenshot()
+    image = np.frombuffer(screenshot, dtype=np.uint8)
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    for i, target in enumerate(targets):
+        rectangle = target.RECTANGLE
+        cropped = image[
+            rectangle.Y_RANGE[0] : rectangle.Y_RANGE[1],
+            rectangle.X_RANGE[0] : rectangle.X_RANGE[1],
+        ]
+        # croppedとtarget.IMAGEのサイズは同じ前提なので、類似度は1x1の配列で返ってくる
+        similarity = cv2.matchTemplate(cropped, cv2.imread(target.IMAGE), cv2.TM_CCOEFF_NORMED)[0][0]
+        if log:
+            print("{}番目のターゲットとの類似度は{}です".format(i, similarity))
+        if similarity > 0.9:
+            return i
+    return -1
+
 async def handle_response(res: Response):
+    if not res.url.startswith("http://w14h.kancolle-server.com/kcsapi/"):
+        return
+    
     global page
-    if res.url == "http://w14h.kancolle-server.com/kcsapi/api_start2/getData":
-        print("ゲームスタートに成功しました")
-        page = Page.START
-    elif res.url == "http://w14h.kancolle-server.com/kcsapi/api_port/port":
+    global wait_for_dock_to_open_task
+    
+    if res.url.endswith("/api_port/port"):
         print("母港に到達しました")
         page = Page.PORT
         
@@ -49,30 +76,75 @@ async def handle_response(res: Response):
             
             print("入渠の必要がある艦がいます")
             
-            await random_sleep(3)
-            
-            # 次に入渠ページに訪れたときの処理を設定
-            async def _handle_in_ndock_page():
+            # 入渠させる処理
+            async def start_repair():
+                # 入渠ボタンをクリック
+                print("入渠ボタンをクリックします")
+                await click(canvas, REPAIR)
+                
+                # 入渠ページに遷移するまで待機
+                print("入渠ページへ遷移するまで待機します")
+                while page != Page.NDOCK:
+                    await asyncio.sleep(0.1)
+                
+                # 入渠ページに到達したら処理を実行
+                print("入渠ページに遷移したので、入居させます")
                 await random_sleep()
+                
+                print("一つ目のドックをクリックします")
                 await click(canvas, repair_dock_button(0))
                 await random_sleep()
+                
+                print("一番上の艦をクリックします")
                 await click(canvas, repair_ship(0))
                 await random_sleep()
+                
+                print("入渠開始ボタンをクリックします")
                 await click(canvas, REPAIR_START)
                 await random_sleep()
+                
+                print("入渠開始確認ボタンをクリックします")
                 await click(canvas, REPAIR_START_CONFIRM)
                 await random_sleep()
+                
+                print("母港画面に戻ります")
                 await click(canvas, HOME_PORT)
-            global handle_in_ndock_page
-            handle_in_ndock_page = lambda: _handle_in_ndock_page()
+                await random_sleep()
+                
+                print("母港画面に戻りました")
             
-            # 入渠ボタンをクリック
-            await click(canvas, REPAIR)
-    elif res.url == "http://w14h.kancolle-server.com/kcsapi/api_get_member/ndock":
+            # 入居させる処理をキューに追加
+            await queue.put(start_repair)
+        else:
+            print("入渠ドックが埋まっているので待機します")
+            print("この処理はコメントアウトしています")
+            # complete_time = ndock[0].get("api_complete_time")
+            # wait_seconds = complete_time / 1000 - time()
+            # async def wait_and_reload():
+            #     print("{}秒待ちます".format(wait_seconds))
+            #     # TODO handle_in_ndock_pageに処理を入れる形で実装する。そのためにはif分岐の順序などを見直す必要がある
+            #     await asyncio.sleep(wait_seconds)
+            #     await random_sleep()
+            #     print("入渠ページへ遷移します")
+            #     await click(canvas, REPAIR)
+            #     await random_sleep()
+            #     print("母港画面へ戻ります")
+            #     await click(canvas, HOME_PORT)
+            # if wait_for_dock_to_open_task is not None:
+            #     wait_for_dock_to_open_task.cancel()
+            # wait_for_dock_to_open_task = asyncio.create_task(wait_and_reload())
+        return
+    
+    if res.url.endswith("/api_start2/getData"):
+        print("getDataのレスポンスを受け取りました")
+    elif res.url.endswith("/api_get_member/ndock"):
         print("入渠ドックに到達しました")
         page = Page.NDOCK
-        await handle_in_ndock_page()
-        handle_in_ndock_page = lambda: asyncio.sleep(0)
+    else:
+        if wait_for_dock_to_open_task is not None:
+            print("関係のないページに遷移したので入渠ドックの待機をキャンセルします")
+            wait_for_dock_to_open_task.cancel()
+            wait_for_dock_to_open_task = None
 
 async def main():
     async with async_playwright() as p:
@@ -89,16 +161,31 @@ async def main():
         )
         
         await random_sleep(5, 3)
-        while page != Page.PORT:
+        # スタートページにいる限り、ゲームスタートボタンの位置を定期的にクリックする
+        while page == Page.START:
             await click(canvas, GAME_START)
             await random_sleep()
+        
+        # 右下に設定ボタンが出現するまで待機する
+        while True:
+            if await scan(canvas, [SETTING_SCAN_TARGET]) == 0:
+                break
+            await asyncio.sleep(1)
         
         print("ゲームスタート処理を正常に終了しました")
         await random_sleep()
         
         while True:
             for i in range(4):
-                print("\rworking" + "." * i + " " * (3 - i), end="")
+                if queue.empty():
+                    # キューが空の場合は待機
+                    print("\rwaiting" + "." * i + " " * (3 - i), end="")
+                else:
+                    # キューが空でない場合は処理を実行
+                    print("\rprocess start!")
+                    coroutine = await queue.get()
+                    await coroutine()
+                # 1秒ごとに確認
                 await asyncio.sleep(1)
 
 asyncio.run(main())
