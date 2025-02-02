8247238cb3268fcedd955d77faa08f9b35741d27
sinano1107 <tftf161107@icloud.com>
2025-02-02 10:25:17 +0900

    delete cursorCapture.js

--- cursorCapture.js
+++
@@ -1,5 +0,0 @@
-// 艦これCanvas内でのカーソルの座標をログに出力する
-// canvasを含むiframeのsrcに書かれているURLにアクセスして、consoleで実行すると使えます
-
-const canvas = document.getElementsByTagName("canvas")[0];
-canvas.onmousemove = (e) => console.log(e.offsetX, e.offsetY);
