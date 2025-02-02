ba059da947fcf5ef4001586c4690f164cfc78dd3
sinano1107 <tftf161107@icloud.com>
2025-02-02 22:44:55 +0900

    add supply.py

--- expedition.py
+++ expedition.py
@@ -4,31 +4,14 @@ from datetime import datetime, timedelta
 from playwright.async_api import async_playwright, Response
 
 from scan.targets.targets import EXPEDITION_DESTINATION_SELECT_SCAN_TARGET, EXPEDITION_NEXT_SCAN_TARGET, HOME_PORT_SCAN_TARGET, SETTING_SCAN_TARGET, SORTIE_SELECT_SCAN_TARGET
-from targets.targets import EXPEDITION_DESTINATION_SELECT_DECIDE, EXPEDITION_DESTINATION_SELECT_TOP, EXPEDITION_SELECT, EXPEDITION_START, FULL_FLEET_SUPPLY, HOME_PORT, REPAIR, SORTIE, SUPPLY
+from targets.targets import EXPEDITION_DESTINATION_SELECT_DECIDE, EXPEDITION_DESTINATION_SELECT_TOP, EXPEDITION_SELECT, EXPEDITION_START, HOME_PORT, REPAIR, SORTIE
 from utils.click import click
 from utils.context import Context, ResponseMemory
 from utils.game_start import game_start
 from utils.page import Page
 from utils.random_sleep import random_sleep
-from utils.rectangle import Rectangle
+from utils.supply import supply
 from utils.wait_until_find import wait_until_find
-
-
-async def supply(fleet_num: int = 1):
-    if fleet_num < 1 or fleet_num > 4:
-        raise ValueError("fleet_numは1以上4以下である必要があります")
-    
-    await click(SUPPLY)
-    await random_sleep()
-    
-    x_start = 45 * fleet_num + 165
-    await click(Rectangle(x_range=(x_start, x_start + 20), y_range=(170, 190), name=f"補給ページの第{fleet_num}艦隊"))
-    await random_sleep()
-    
-    await click(FULL_FLEET_SUPPLY)
-    await random_sleep()
-    await click(HOME_PORT)
-    await wait_until_find(SETTING_SCAN_TARGET)
 
 
 async def go_expedition():
---
+++ utils/supply.py
@@ -0,0 +1,23 @@
+from scan.targets.targets import SETTING_SCAN_TARGET
+from targets.targets import FULL_FLEET_SUPPLY, HOME_PORT, SUPPLY
+from utils.click import click
+from utils.random_sleep import random_sleep
+from utils.rectangle import Rectangle
+from utils.wait_until_find import wait_until_find
+
+
+async def supply(fleet_num: int = 1):
+    if fleet_num < 1 or fleet_num > 4:
+        raise ValueError("fleet_numは1以上4以下である必要があります")
+    
+    await click(SUPPLY)
+    await random_sleep()
+    
+    x_start = 45 * fleet_num + 165
+    await click(Rectangle(x_range=(x_start, x_start + 20), y_range=(170, 190), name=f"補給ページの第{fleet_num}艦隊"))
+    await random_sleep()
+    
+    await click(FULL_FLEET_SUPPLY)
+    await random_sleep()
+    await click(HOME_PORT)
+    await wait_until_find(SETTING_SCAN_TARGET)
