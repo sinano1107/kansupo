bb04e39d2cb41e53a946491a93608a7e16ce932b
sinano1107 <tftf161107@icloud.com>
2025-02-11 21:10:07 +0900

    change clickで座標を表示しないように変更

--- utils/click.py
+++ utils/click.py
@@ -7,4 +7,4 @@ async def click(target=Rectangle(x_range=(0, 1200), y_range=(0, 720))):
     x, y = target.random_point()
     await Context.canvas.click(position={"x": x, "y": y})
     name = target.NAME + "を" if target.NAME else ""
-    print(f"{name}クリックしました {x=} {y=}")
+    print(f"{name}クリックしました")
